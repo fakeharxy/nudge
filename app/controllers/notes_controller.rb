f6cbb29494c313472893af8cf0e3d03e30485070
harxy <harkerp@gmail.com>
2018-02-06 10:35:33 +0000

    models set up

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -69,6 +69,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:body, :todo_by, :last_seen)
+      params.require(:note).permit(:body, :todo_by, :primary) 
     end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,7 @@
 class Note < ApplicationRecord
+  has_many :note_tags
+  has_many :tags, -> { uniq }, through: :note_tags
+
+  has_one :primary_note_tag, -> { where primary: true }, class_name: "NoteTag"
+  has_one :primary_tag, through: :primary_note_tag, source: :tag
 end
