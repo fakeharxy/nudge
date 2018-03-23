7e4f9e87bada4274fa52e0ffd67f352774b828d2
fakeharxy <harkerp@gmail.com>
2018-03-23 11:01:32 +0000

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_urgency
+  
+  def set_urgency
+    @urgencies = current_user.notes.return_urgencies
+  end
 end
