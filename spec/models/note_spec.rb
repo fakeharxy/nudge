c332c7abfd30f64f0abb0e67a803adf98bc07720
fakeharxy <harkerp@gmail.com>
2018-04-09 10:37:20 +0100

    rebuilt tests

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -17,86 +17,40 @@ RSpec.describe Note, type: :model do
     expect(@bob.seentoday).to eq(false)
   end
 
-  it 'accepts a primary tag' do
-    @bob.add_tag("hello", user_id: @user.id)
+  it 'accepts a tag' do
+    @bob.add_tag("hello", @user.id)
     expect(@bob.get_tag_name).to eq("hello")
   end
 
-  it 'can return both primary and secondary tags' do
+  it 'can return both tags' do
     expect(@bob.get_all_tag_names).to eq(["tag", "second"])
   end
 
   it 'deletes a tag when the note is deleted' do
-    @bob.add_secondary_tags("hello, this, tags", @user.id)
+    @bob.add_second("hello", @tag.id, @user.id)
     @bob.destroy_with_tags
     expect(Tag.all).to eq([])
   end
 
   it 'can determine order from importance' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 7
+    @bob.add_tag("mike", @user.id)
+    @bob.tag.importance = 7
     @bob.set_last_seen = 2.days.ago
-    expect(@bob.urgency).to eq(42)
-  end
-
-  it 'can return the urgency, red' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 7
-    @bob.set_last_seen = 2.days.ago
-    expect(@bob.urgency_level).to eq('high')
+    expect(@bob.urgency).to eq(15)
   end
 
   it 'can return the urgency, amber' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 4
-    @bob.set_last_seen = 2.days.ago
+    @bob.add_tag("mike", @user.id)
+    @bob.tag.importance = 4
+    @bob.set_last_seen = 8.days.ago
     expect(@bob.urgency_level).to eq('medium')
   end
 
   it 'can return the urgency, green' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 2
+    @bob.add_tag("mike", @user.id)
+    @bob.tag.importance = 2
     @bob.set_last_seen = 2.days.ago
     expect(@bob.urgency_level).to eq('low')
-  end
-
-  it 'will be the importance value if the date seen is today' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 3
-    @bob.set_last_seen = Date.today
-    expect(@bob.urgency).to eq(3)
-  end
-
-  it 'will be able to provide the most urgent note' do
-    @bob.add_primary_tag("mike", @user.id)
-    @bob.primary_tag.importance = 7
-    @bob.set_last_seen = 2.days.ago
-    @mike = Note.create!(body: "Mike")
-    @mike.add_primary_tag("donkey", @user.id)
-    @mike.primary_tag.importance = 6
-    @mike.set_last_seen = 2.days.ago
-    expect(Note.get_most_urgent).to eq(@bob)
-  end
-
-  it 'will be able to find the most urgent note part 2' do
-    @bob.add_primary_tag("hello", @user.id)
-    @bob.primary_tag.importance = 3
-    @bob.set_last_seen = 2.days.ago
-    @trey = Note.create!(body: "Trey")
-    @trey.add_primary_tag("donkey", @user.id)
-    @trey.primary_tag.importance = 3
-    @trey.set_last_seen = 3.days.ago
-    expect(Note.get_most_urgent).to eq(@trey)
-  end
-
-  it 'does not delete a tag if connected to another note' do
-    mike = Note.create!
-    @bob.add_secondary_tags("hello,", @user.id)
-    mike.add_secondary_tags("hello,", @user.id)
-    @bob.destroy_with_tags
-    expect(Tag.all).to_not eq([])
-    mike.destroy_with_tags
-    expect(Tag.all).to eq([])
   end
 
   it "can mark a note as seen & update last_seen" do
@@ -106,31 +60,4 @@ RSpec.describe Note, type: :model do
     expect(@bob.seentoday).to eq(true)
   end
 
-  it "can tell if every note is seen" do
-    @bob.mark_as_seen
-    mike = Note.create!(body: "mike", user_id: @user.id)
-    mike.mark_as_seen
-    expect(Note.all_seen?(@user.id)).to eq(true)
-  end
-
-  it "can tell if every note is not seen" do
-    @bob.mark_as_seen
-    mike = Note.create!(body: "mike", user_id: @user.id)
-    mike.mark_as_seen
-    expect(Note.all_seen?(@user.id)).to eq(true)
-  end
-
-  it "can get the most recent note" do
-    mike = Note.create!(body: "mike", user_id: @user.id, last_seen: 2.days.ago)
-    expect(Note.most_recent(@user.id)).to eq(@bob)
-  end
-
-  it 'can reset seen_today' do
-    @bob.mark_as_seen
-    @trey = Note.create!(body: "Trey", user_id: @user.id)
-    @trey.mark_as_seen
-    Note.reset_seen_status(@user.id)
-    expect(@bob.reload.seentoday).to eq(false)
-    expect(@trey.reload.seentoday).to eq(false)
-  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,20 +7,22 @@ RSpec.describe Tag, type: :model do
   end
 
   before(:each) do
-    @bob = Note.create!(body: "bob", user_id: @user.id)
-    @bob.add_primary_tag("hello", @user.id)
+    @tag = Tag.create!(name: "tag", user_id: @user.id)
+    @second = Second.create!(name: "second", tag_id: @tag.id, user_id: @user.id)
+    @bob = Note.create!(body: "bob", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @bob.add_tag("hello", @user.id)
     @bobtag = Tag.find_by(name: "hello")
   end
 
   it 'has default importance of 5' do
-    expect(@bob.primary_tag.importance).to eq(5)
+    expect(@bob.tag.importance).to eq(5)
   end
 
   it 'does not overwrite existing importance' do
-    @mike = Note.create!(body: "mike", user_id: @user.id)
-    @bob.primary_tag.set_importance = 7
-    @mike.add_primary_tag("hello", @user.id)
-    expect(@mike.primary_tag.importance).to eq(7)
+    @mike = Note.create!(body: "mike", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @bob.tag.set_importance = 7
+    @mike.add_tag("hello", @user.id)
+    expect(@mike.tag.importance).to eq(7)
   end
 
   it 'can get all notes it belongs to' do
@@ -28,36 +30,25 @@ RSpec.describe Tag, type: :model do
   end
 
   it 'can get all primary notes' do
-    @mike = Note.create!(body: "mike", user_id: @user.id)
-    @mike.add_secondary_tags("snacks", @user.id)
-    expect(Tag.get_all_primary_tags(@user.id)).to eq([@bobtag])
+    @mike = Note.create!(body: "mike", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @mike.add_second("snacks", @tag.id, @user.id)
+    expect(Tag.get_all_tags(@user.id)).to eq([@bobtag])
   end
 
   it 'can return number of notes' do
-    @mike = Note.create!(body: "mike", user_id: @user.id)
-    @mike.add_secondary_tags("hello", @user.id)
-    @tim = Note.create!(body: "tim", user_id: @user.id)
-    @tim.add_primary_tag("hello", @user.id)
-    @tim.add_secondary_tags("herro,my,friend", @user.id)
-    expect(Tag.find_by(name:"hello").note_count).to eq(3)
+    @mike = Note.create!(body: "mike", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @mike.add_second("hello", @tag.id, @user.id)
+    @tim = Note.create!(body: "tim", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @tim.add_tag("hello", @user.id)
+    @tim.add_second("herro", @tag.id, @user.id)
+    expect(Tag.find_by(name:"hello").note_count).to eq(2)
   end
 
   it 'can get multiple notes(with secondary tags)' do
-    @trey = Note.create!(body: "trey", user_id: @user.id)
-    @trey.add_primary_tag("hello", @user.id)
-    @mike = Note.create!(body: "mike", user_id: @user.id)
-    @mike.add_secondary_tags("hello", @user.id)
-    expect(@bobtag.get_all_notes).to eq([@bob,@trey,@mike])
+    @trey = Note.create!(body: "trey", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @trey.add_tag("hello", @user.id)
+    @mike = Note.create!(body: "mike", tag_id: @tag.id, second_id: @second.id, user_id: @user.id)
+    @mike.add_second("hello", @tag.id, @user.id)
+    expect(@bobtag.get_all_notes).to eq([@bob,@trey])
   end
-
-  it 'can get multiple notes in order' do
-    @trey = Note.create!(body: "trey", user_id: @user.id)
-    @trey.add_primary_tag("hello", @user.id)
-    @treytag = Tag.find_by(name:"hello")
-    @mike = Note.create!(body: "mike", user_id: @user.id)
-    @mike.add_secondary_tags("monkey", @user.id)
-    @miketag = Tag.find_by(name:"monkey")
-    expect(Tag.in_order_of_most_used(@user.id)).to eq ([@treytag, @miketag])
-  end
-
 end
