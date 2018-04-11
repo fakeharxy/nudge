e60c0a4b2d95443dbc47a01f5b6bb2dd7045ecb9
fakeharxy <harkerp@gmail.com>
2018-04-11 17:12:06 +0100

    bug fix?

--- db/migrate/20180411154258_remove_tag_id_second_id_from_complete.rb
+++ db/migrate/20180411154258_remove_tag_id_second_id_from_complete.rb
@@ -1,6 +1,6 @@
 class RemoveTagIdSecondIdFromComplete < ActiveRecord::Migration[5.0]
   def change
-    remove_column :completes, :tag_id, :string
-    remove_column :completes, :second_id, :string
+    remove_column :completes, :tag_id, :integer
+    remove_column :completes, :second_id, :integer
   end
 end
