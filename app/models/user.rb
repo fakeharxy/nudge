7b81d6961f96c63509a6a6f8912406b855ee9d03
fakeharxy <harkerp@gmail.com>
2018-04-27 10:55:38 +0100

    styles n' stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
   has_many :notes
   has_many :tags
   has_many :seconds
+  after_create :fill_db
 
   def tags_in_order_of_most_used
     tags.all.sort_by{|i| - i.note_count}
+  end
+
+  def fill_db
   end
 end
